2917c5fb08b3b17914a6dca0721d2758213f2787
Your Name <you@example.com>
2023-12-22 08:39:08 +0100

    Update fork

--- planner/src/rrt_star/main.cpp
+++ planner/src/rrt_star/main.cpp
@@ -29,25 +29,9 @@ int main(int argc, char **argv)
     {
         sleep(0.01);
     }
-    // m->set_boundary(60, 40);
-    // std::vector<KDPoint> points;
-    // for (int i = 0; i < 30; ++i)
-    // {
-    //     KDPoint p = {20, double(i)};
-    //     points.push_back(p);
-    // }
 
-    // for (int i = 0; i < 30; ++i)
-    // {
-    //     KDPoint p = {40, 40 - double(i)};
-    //     points.push_back(p);
-    // }
-
-    // m->set_obstacle(points);
     KDPoint point = {0,0};
     m->set_start(point);
-    // point = {-0.8, -6.3};
-    // m->set_end(point);
 
     if (m->_show_graphics)
         m->ShowMap();
