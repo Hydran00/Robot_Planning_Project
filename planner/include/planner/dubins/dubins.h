494a003f9207db748b33faa1685d2e9c8a084c45
Davide <ndavide.dn@gmail.com>
2024-01-05 09:38:13 +0100

    Added dubins generator

---
+++ planner/include/planner/dubins/dubins.h
@@ -0,0 +1,32 @@
+#ifndef DUBINS_PATH_H
+#define DUBINS_PATH_H
+
+#include <cmath>
+#include <vector>
+
+class DubinsPath
+{
+private:
+    std::vector<std::vector<double>> _s;
+    std::vector<std::vector<double>> _e;
+    double _r;
+    std::vector<std::vector<std::vector<double>>> _paths;
+
+public:
+    DubinsPath(std::vector<double> start, std::vector<double> end, double r = 1.0);
+
+    std::vector<std::vector<std::vector<double>>> calc_paths();
+    std::vector<std::vector<double>> get_shortest_path();
+
+private:
+    std::vector<double> calc_end();
+    double mod2pi(double theta);
+    std::vector<std::vector<double>> calc_lsl_from_origin(std::vector<double> e);
+    std::vector<std::vector<double>> calc_rsr_from_origin(std::vector<double> e);
+    std::vector<std::vector<double>> calc_lsr_from_origin(std::vector<double> e);
+    std::vector<std::vector<double>> calc_rsl_from_origin(std::vector<double> e);
+    std::vector<std::vector<double>> calc_lrl_from_origin(std::vector<double> e);
+    std::vector<std::vector<double>> calc_rlr_from_origin(std::vector<double> e);
+};
+
+#endif // DUBINS_PATH_H
